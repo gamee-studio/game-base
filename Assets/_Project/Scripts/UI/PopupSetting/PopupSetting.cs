802666b9096b5c5170de9cb06f21b83b57bc2684
guardian.of.gods99@gmail.com <guardian.of.gods99@gmail.com>
2023-07-26 14:44:04 +0700

    -fix: ui setting

--- Assets/_Project/Scripts/UI/PopupSetting/PopupSetting.cs
+++ Assets/_Project/Scripts/UI/PopupSetting/PopupSetting.cs
@@ -1,10 +1,19 @@
-using Pancake.IAP;
+using UnityEngine;
 #if UNITY_IOS
 using UnityEngine.iOS;
 #endif
 
 public class PopupSetting : Popup
 {
+    public GameObject btnRestorePurchased;
+
+    void Start()
+    {
+        #if UNITY_ANDROID
+            btnRestorePurchased.SetActive(false);
+        #endif
+    }
+    
     public void OnClickRestorePurchase()
     {
         #if UNITY_ANDROID
